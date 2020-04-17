26f67a99bb4dabcae41020696012db6de2c1a7c5
Kersum10 <umasi.sandy@gmail.com>
2020-04-17 04:18:58 +0200

    .

--- Scripts/activaAmpleDeBanda.sh
+++ Scripts/activaAmpleDeBanda.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 prueba() {
-#echo "hola mundo"
-echo $1 
+#echo "Esto está COMPLICATED"
+tc qdisc add dev $1 root handle 1: prio bands 8
 }
 
 #tc qdisc per eth0
 prueba eth0
+
+#Para probar, se puede ejecutar en cada consola.
+#watch -n 1 tc -s qdisc show dev eth0
+#watch -n 1 tc -s qdisc show dev eth1
