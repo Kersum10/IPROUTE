570b9ed729761e6f58b865a5c911ede8140978c8
Kersum10 <umasi.sandy@gmail.com>
2020-04-20 18:02:52 +0200

    probar

---
+++ Scripts/Conf_departament.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ip route add default gw 192.168.2.1 dev eth0
--- Scripts/modificaAmpleDeBandaPerSubxarxa.sh
+++ Scripts/modificaAmpleDeBandaPerSubxarxa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tc qdisc add dev eth1 parent 1:$1 handle 1: tbf rate $2 latency 50ms burst 1540
-#tc qdisc add dev eth0 parent 1:$1 handle 1: tbf rate $2 latency 50ms burst 1540
+tc qdisc add dev eth0 parent 1:$1 handle 1: tbf rate $2 latency 50ms burst 1540
 
 echo "$1 cambiado $2"
 
